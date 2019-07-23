7688250a2136615480fb47dcd98b15092a395292
erozonachi <erozonachi.james@gmail.com>
2019-07-23 16:51:58 +0100

    Add create function to the tasks-controller

--- resources/tasks/tasks-controller.js
+++ resources/tasks/tasks-controller.js
@@ -1,3 +1,17 @@
 const model = require('./tasks-model');
 
-module.exports = {};
+module.exports = {
+  create: async (req, res) => {
+    try {
+      const newTask = await model.create(req.body);
+
+      res.status(201)
+        .json(newTask);
+    } catch(error) {
+      res.status(500)
+        .json({
+          error: 'Server error'
+        });
+    }
+  }
+};
