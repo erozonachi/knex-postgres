48bc760dc3fe0d6b01120833b3389799b10ad1ce
erozonachi <erozonachi.james@gmail.com>
2019-07-23 16:59:33 +0100

    Add update function to the tasks-controller

--- resources/tasks/tasks-controller.js
+++ resources/tasks/tasks-controller.js
@@ -27,5 +27,20 @@ module.exports = {
           error: 'Server error'
         });
     }
-  }
+  },
+
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const task = await model.update(id, req.body);
+
+      res.status(200)
+        json(task);
+    } catch(error) {
+      res.status(500)
+        .json({
+          error: 'Server error'
+        });
+    }
+  },
 };
