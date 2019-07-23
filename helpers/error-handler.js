a753d71859ac827c701276e84604d413845d6604
erozonachi <erozonachi.james@gmail.com>
2019-07-23 21:20:32 +0100

    Add handle-error function to the error-handler

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -1,3 +1,12 @@
 module.exports = {
-
+  handleError: (err, req, res) => {
+    if(err.code === '23505' && err.detail.includes('title')) {
+      return res.status(400)
+        .json({ message: 'Supplied title field already exists' });
+    }
+    res.status(500)
+      .json({
+        error: 'Server error'
+      });
+  }
 }
